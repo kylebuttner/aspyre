d722bc785e1f0ee763bb46d49f83172702815d4f
Claudia Green <claudia@claudiag.me.uk>
2016-05-03 14:31:50 +0100

    Implemented initial controller. Dummy 'goals' data displaying on the index page. 2/2 tests passing.

--- test/e2e/goalbusterFeatures.js
+++ test/e2e/goalbusterFeatures.js
@@ -1,6 +1,17 @@
 describe('Goalbuster App', function() {
+
+
   it('has a title', function() {
     browser.get('/');
     expect(browser.getTitle()).toEqual('Goalbuster App');
   });
+
+  it('displays goals', function() {
+    browser.get('/');
+    var goals = $$('#goals p');
+    expect(goals.first().getText()).toEqual('Goal 1');
+    expect(goals.last().getText()).toEqual('Goal 3');
+  });
+
+
 });
