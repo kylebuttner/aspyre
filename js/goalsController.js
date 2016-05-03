db991ed2c582821e7f1288109153f471ea72d27f
Claudia Green <claudia@claudiag.me.uk>
2016-05-03 16:57:35 +0100

    Factory created and implemented. All tests passing.

--- js/goalsController.js
+++ js/goalsController.js
@@ -1,10 +1,12 @@
-goalbusterApp.controller('GoalbusterController', [ function() {
+goalbusterApp.controller('GoalbusterController', [ 'GoalFactory', function(GoalFactory) {
   var self = this;
 
-  self.goals = ["Goal 1", "Goal 2", "Goal 3"]
+  self.goals = [new GoalFactory("Goal 1"),
+                new GoalFactory("Goal 2"),
+                new GoalFactory("Goal 3")]
 
   self.addNewGoal = function(newGoal) {
-    self.goals.push(newGoal);
+    self.goals.push(new GoalFactory(newGoal));
   };
-  
+
 }]);
