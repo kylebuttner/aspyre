182a731bdcd3a45192599af1e4c5a53bc44f379c
hanfak <fakira.work@gmail.com>
2016-05-04 10:28:43 +0100

    tested service for getting data

--- test/unit/goalService.spec.js
+++ test/unit/goalService.spec.js
@@ -1,24 +1,25 @@
-// describe('goalService', function() {
-//   beforeEach(module('goalbusterApp'));
+describe('GoalService', function() {
+  beforeEach(module('goalbusterApp'));
 
-//   var goalService, GoalFactory, httpBackend;
-//   var apiURL = "http://goalbuster-api.herokuapp.com/";
-//   var goalsData = [{name: "learn piano"}, {name: "finish book"}, {name: "more sport"} ] 
+  var goalService, GoalFactory, httpBackend;
+  var apiURL = "http://goalbuster-api.herokuapp.com/";
+  var goalsData = [{name: "learn piano"}, {name: "finish book"}, {name: "more sport"} ]
 
-//   beforeEach(inject(function(_goalService_, _GoalFactory_, $httpBackend){
-//     goalService = _goalService_;
-//     GoalFactory = _GoalFactory_;
-//     httpBackend = $httpBackend;
-//   }));
+  beforeEach(inject(function(_GoalService_, _GoalFactory_, $httpBackend){
+    GoalService = _GoalService_;
+    GoalFactory = _GoalFactory_;
+    httpBackend = $httpBackend;
+  }));
 
-//   it ('fetches a list of goals', function(){
-//     httpBackend.expectGet(apiURL + "goals").respond(goalsData);
-//     var goal1 = new GoalFactory("learn piano");
-//     var goal2 = new GoalFactory("finish book");
+  it ('fetches a list of goals', function(){
+    httpBackend.expectGET(apiURL + "goals.json").respond(goalsData);
+    var goal1 = new GoalFactory("learn piano");
+    var goal2 = new GoalFactory("finish book");
+    var goal3 = new GoalFactory("more sport");
 
-//     goalService.getAll().then(function(goals){
-//       expect(goals).toEqual([goal1, goal2])
-//     });
-//     httpBackend.flush();
-//   }); 
-// });
+    GoalService.getAll().then(function(goals){
+      expect(goals).toEqual([goal1, goal2, goal3])
+    });
+    httpBackend.flush();
+  });
+});
