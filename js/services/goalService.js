cb83113a005732b599477595eabe2a820b5751de
hanfak <fakira.work@gmail.com>
2016-05-04 10:28:43 +0100

    tested service for getting data

--- js/services/goalService.js
+++ js/services/goalService.js
@@ -1,4 +1,15 @@
-goalbusterApp.service('GoalService', ['GoalFactory', function(GoalFactory) {
+goalbusterApp.service('GoalService', ['$http', 'GoalFactory', function($http, GoalFactory) {
+  var self = this;
 
+  self.getAll = function() {
+    return $http.get('http://goalbuster-api.herokuapp.com/goals.json')
+      .then(_handleResponseFromApi);
+  }
+
+  function _handleResponseFromApi (response)  {
+    return response.data.map(function(goalsData){
+      return new GoalFactory(goalsData.name);
+    });
+  }
 
 }]);
