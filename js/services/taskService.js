01d36f7f87cf503d842de4bf0e9cde5fb32b7db3
Kyle Buttner <kylebuttner@gmail.com>
2016-05-05 17:30:49 +0100

    added update and delete functionality for tasks

--- js/services/taskService.js
+++ js/services/taskService.js
@@ -3,7 +3,7 @@
 goalbusterApp.service('TaskService',['TaskFactory', '$http', function(TaskFactory, $http){
   self = this;
 
-  
+
   self.getAllFromApi = function(goalId){
         return $http.get('http://goalbuster-api.herokuapp.com/goals/'+ goalId +'/tasks.json')
     .then(_handleResponseFromApi);
@@ -22,10 +22,26 @@ goalbusterApp.service('TaskService',['TaskFactory', '$http', function(TaskFactor
   }
 
   self.editTaskOnApi = function(data, taskId){
+    return $http({
+      method: 'PUT',
+      url: 'http://goalbuster-api.herokuapp.com/tasks/' + taskId,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      data: _formatPOSTData(data)
+    });
   }
 
   self.deleteTaskOnApi = function(taskId){
-
+    return $http({
+      method: 'DELETE',
+      url: 'http://goalbuster-api.herokuapp.com/tasks/' + taskId,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+    });
   }
 
   function _handleResponseFromApi(response) {
