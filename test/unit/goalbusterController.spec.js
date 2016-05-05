341c68ea4a6bc453fecab9bd085942c2918610a5
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-05-05 14:25:17 +0100

    added controller function and tests for CR tasks

--- test/unit/goalbusterController.spec.js
+++ test/unit/goalbusterController.spec.js
@@ -3,29 +3,52 @@
 describe('GoalbusterController', function() {
   beforeEach(module('goalbusterApp'));
 
-  var ctrl, httpBackend, GoalService, GoalFactory, testObject, q;
+  var ctrl, httpBackend, GoalService, GoalFactory, GoalObject, TaskName, TaskService, q, GoalId;
 
-  beforeEach(inject(function($controller, _GoalFactory_, _GoalService_, $httpBackend, $q) {
+  beforeEach(inject(function($controller, _GoalFactory_, _GoalService_, _TaskService_, $httpBackend, $q) {
     GoalFactory = _GoalFactory_;
     GoalService = _GoalService_;
-    testObject = [{name: "learn piano"}];
+    TaskService = _TaskService_;
+    GoalObject =  "learn piano"
+    TaskName = "read notes"
+    GoalId = 1
     ctrl = $controller('GoalbusterController');
     q = $q;
   }));
 
-  it('when initializing it gets all goals from API', function() {
-    expect(ctrl.goals).toEqual([]);
+  describe('CRUD Goal', function() {
+    xit('when initializing it gets all goals from API', function() {
+      expect(ctrl.goals).toEqual([]);
+    });
+
+    it('can add a new goal', function() {
+      spyOn(GoalService, "postGoalToApi");
+      ctrl.addNewGoal(GoalObject)
+      expect(GoalService.postGoalToApi).toHaveBeenCalledWith(GoalObject);
+    });
+
+    it('refreshes the list of goals', function() {
+      spyOn(GoalService, "getAllFromApi").and.returnValue(q.when())
+      ctrl.refreshGoals()
+      expect(GoalService.getAllFromApi).toHaveBeenCalled();
+    });
   });
 
-  it('can add a new goal', function() {
-    spyOn(GoalService, "postGoalToApi");
-    ctrl.addNewGoal(testObject)
-    expect(GoalService.postGoalToApi).toHaveBeenCalledWith(testObject);
-  });
+  describe('CRUD Task', function (){
+    xit('when initializing it gets all tasks from API', function() {
+      expect(ctrl.tasks).toEqual([]);
+    });
+  
+    it('can add a new task', function () {
+      spyOn(TaskService, 'postTaskToApi');
+      ctrl.addNewTask(TaskName, GoalId);
+      expect(TaskService.postTaskToApi).toHaveBeenCalledWith(TaskName, GoalId);
+    });
 
-  it('refreshes the list of goals', function() {
-    spyOn(GoalService, "getAllFromApi").and.returnValue(q.when())
-    ctrl.refreshGoals()
-    expect(GoalService.getAllFromApi).toHaveBeenCalled();
+    it('refreshes the list of goals', function() {
+      spyOn(TaskService, "getAllFromApi").and.returnValue(q.when())
+      ctrl.refreshTasks(GoalId)
+      expect(TaskService.getAllFromApi).toHaveBeenCalledWith(GoalId);
+    });
   });
 });
