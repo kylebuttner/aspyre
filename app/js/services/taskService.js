8fdb1e16a3328b61b855a53a8277114d79d41b8e
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-05-05 16:28:50 +0100

    added create methods to controller and tests

--- app/js/services/taskService.js
+++ app/js/services/taskService.js
@@ -1,10 +1,39 @@
 "use strict"
 
-goalbusterApp.service('TaskService' ,[ function(){
+goalbusterApp.service('TaskService',['TaskFactory', '$http', function(TaskFactory, $http){
   self = this;
 
-  self.postTaskToApi = function(){}
   
-  self.getAllFromApi = function(){}
-  
+  self.getAllFromApi = function(goalId){
+        return $http.get('http://goalbuster-api.herokuapp.com/goals/'+ goalId +'/tasks.json')
+    .then(_handleResponseFromApi);
+  }
+
+  self.postTaskToApi = function(data, goalId){
+    return $http({
+      method: 'POST',
+      url: 'http://goalbuster-api.herokuapp.com/goals/' + goalId + "/tasks",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      data: _formatPOSTData(data)
+    });
+
+  }
+
+  function _handleResponseFromApi(response) {
+    return response.data.map(_createTaskFromData);
+  };
+
+  function _createTaskFromData(TaskData) {
+    return new TaskFactory(TaskData.name)
+  }
+
+  function _formatPOSTData(NewTask) {
+    var data = {name: NewTask};
+    return JSON.stringify(data);
+  }
+
+
 }])
--- test/unit/taskService.spec.js
+++ test/unit/taskService.spec.js
@@ -1,5 +1,37 @@
 // "use strict"
 
 describe('TaskService', function() {
-  
-})
+  beforeEach(module('goalbusterApp'));
+
+  var TaskService, httpBackend;
+  var apiURL = "http://goalbuster-api.herokuapp.com/goals/"
+  var taskData = [{name: "read notes"}, {name: "buy bike"}]
+  var task = "new task"
+  var goalId =  "1"
+
+  beforeEach(inject(function(_TaskService_, _TaskFactory_, $httpBackend){
+    TaskService = _TaskService_;
+    TaskFactory =  _TaskFactory_;
+    httpBackend = $httpBackend;
+  }));
+
+  it ('featch a list of tasks for a goal', function() {
+    httpBackend.expectGET(apiURL + goalId + "/tasks.json" ).respond(taskData)
+    var task1 = new TaskFactory("read notes")
+    var task2 = new TaskFactory("buy bike")
+
+    TaskService.getAllFromApi(goalId).then(function(tasks){
+       expect(tasks).toContain(task1, task2);
+    });
+    httpBackend.flush();    
+  });
+
+  it ('sends a post request', function() {
+    httpBackend.expectPOST(apiURL + goalId +"/tasks").respond(201, 'success');
+    TaskService.postTaskToApi(task, goalId).then(function(response){
+      expect(response.status).toEqual(201);
+    });
+    httpBackend.flush();
+  })
+
+});
