e507d727a13d8380a4bc7690b48d8dffa9833d44
Kyle Buttner <kylebuttner@gmail.com>
2016-05-06 14:57:50 +0100

    added ipCookie

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
-var goalbusterApp = angular.module('goalbusterApp', ['ng-token-auth'])
-
-.config(function($authProvider) {
-       $authProvider.configure({
-           apiUrl: 'http://goalbuster-api.herokuapp.com'
-           authProviderPaths: {
-             github: '/auth/github'
-           }
+var goalbusterApp = angular.module('goalbusterApp', ['ipCookie', 'ng-token-auth'])
+  .config(function($authProvider) {
+         $authProvider.configure({
+             apiUrl: 'http://goalbuster-api.herokuapp.com',
+             authProviderPaths: {
+               github: '/auth/github'
+             }
+         });
        });
-     });
