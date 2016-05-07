d46db25317d732fc3012205572712f517ef72b14
hanfak <fakira.work@gmail.com>
2016-05-07 13:41:06 +0100

    split html into partials

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 var goalbusterApp = angular.module('goalbusterApp', ['ipCookie', 'ng-token-auth'])
   .config(function($authProvider) {
-         $authProvider.configure({
-             apiUrl: 'http://goalbuster-api.herokuapp.com',
-             authProviderPaths: {
-               github: '/auth/github'
-             }
-         });
-       });
+    $authProvider.configure({
+       apiUrl: 'http://goalbuster-api.herokuapp.com',
+       authProviderPaths: {
+         github: '/auth/github'
+       }
+    });
+  });
+  // .directive('goalsInput', function(){
+  //   return {
+  //     restrict: 'A',
+  //     templateUrl: "js/partials/_goalsInput.html",
+  //     scope: true
+  //   };
+  // });
