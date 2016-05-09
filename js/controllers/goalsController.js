3375917f5f6ca3e111033492d7a9cb52f7eb8496
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-05-09 08:12:03 +0100

    migrated user functions to user ctrl. added ctrl to index

--- js/controllers/goalsController.js
+++ js/controllers/goalsController.js
@@ -21,22 +21,6 @@ goalbusterApp.controller('GoalsController', ['ipCookie', '$q', 'GoalsFactory','G
     _refreshGoals();
   };
 
-  self.handleBtnClick = function() {
-    $auth.authenticate("github");
-  };
-
-  self.handleRegBtnClick = function() {
-    $auth.submitRegistration(self.registrationForm);
-  };
-
-  self.handleSignOutBtnClick = function() {
-    $auth.signOut();
-  };
-
-  self.handleLoginBtnClick = function() {
-    $auth.submitLogin($scope.loginForm);
-  };
-
   function _refreshGoals() {
      GoalsService.getAllFromApi().then(_saveGoals)
   };
