404778a62acca123b2d850bca9e9376b174660c1
Kyle Buttner <kylebuttner@gmail.com>
2016-05-09 19:20:51 +0100

    integrate xeditable to allow inline text editing

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,7 @@
-var goalbusterApp = angular.module('goalbusterApp', ['ngAnimate', 'ui.bootstrap', 'ipCookie', 'ng-token-auth'])
+var goalbusterApp = angular.module('goalbusterApp', ['xeditable', 'ngAnimate', 'ui.bootstrap', 'ipCookie', 'ng-token-auth'])
+  .run(function(editableOptions) {
+    editableOptions.theme = 'default';
+  })
   .config(function($authProvider) {
     $authProvider.configure({
        apiUrl: 'https://goalbuster-api.herokuapp.com',
