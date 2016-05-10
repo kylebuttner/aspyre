180b7a3cf828bf08a664bcf4ed747f2234dabe86
Kyle Buttner <kylebuttner@gmail.com>
2016-05-10 14:58:33 +0100

    added ngRoute

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,15 @@
-var goalbusterApp = angular.module('goalbusterApp', ['xeditable', 'ngAnimate', 'ui.bootstrap', 'ipCookie', 'ng-token-auth'])
+var goalbusterApp = angular.module('goalbusterApp', ['ngRoute', 'xeditable', 'ngAnimate', 'ui.bootstrap', 'ipCookie', 'ng-token-auth'])
   .run(function(editableOptions) {
     editableOptions.theme = 'default';
   })
-  .config(function($authProvider) {
+  .config(['$authProvider', '$routeProvider', function($authProvider, $routeProvider) {
     $authProvider.configure({
        apiUrl: 'https://goalbuster-api.herokuapp.com',
        authProviderPaths: {
          github: '/auth/github'
        }
     });
-  });
+    $routeProvider
+      .when("/addnewgoal", {templateUrl: 'partials/addnewgoal.html'})
+      .otherwise({redirectTo:'/'});
+  }]);
