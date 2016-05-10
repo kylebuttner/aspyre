ca2db3cae9e26073b616ff04912737943e7f94ef
Kyle Buttner <kylebuttner@gmail.com>
2016-05-10 17:01:59 +0100

    added ng-enter directive

--- js/app.js
+++ js/app.js
@@ -2,6 +2,19 @@ var goalbusterApp = angular.module('goalbusterApp', ['ngRoute', 'xeditable', 'ng
   .run(function(editableOptions) {
     editableOptions.theme = 'default';
   })
+  .directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+  })
   .config(['$authProvider', '$routeProvider', function($authProvider, $routeProvider) {
     $authProvider.configure({
        apiUrl: 'https://goalbuster-api.herokuapp.com',
