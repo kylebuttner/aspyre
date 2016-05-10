80ea99bc1e8d027e6dbd55b5608f69d7e230f5f5
hanfak <fakira.work@gmail.com>
2016-05-10 16:16:07 +0100

    use ui-router to display views

--- js/app.js
+++ js/app.js
@@ -1,20 +1,24 @@
-var goalbusterApp = angular.module('goalbusterApp', ['ngRoute', 'xeditable', 'ngAnimate', 'ui.bootstrap', 'ipCookie', 'ng-token-auth'])
+var goalbusterApp = angular.module('goalbusterApp', ['ngRoute', 'xeditable', 'ngAnimate', 'ui.bootstrap', 'ipCookie', 'ng-token-auth', 'ui.router'])
   .run(function(editableOptions) {
     editableOptions.theme = 'default';
   })
-  .config(['$authProvider', '$routeProvider', function($authProvider, $routeProvider) {
+  .config(['$authProvider', '$stateProvider', function($authProvider, $stateProvider) {
     $authProvider.configure({
        apiUrl: 'https://goalbuster-api.herokuapp.com',
        authProviderPaths: {
          github: '/auth/github'
        }
     });
-    $routeProvider
-      .when("/", {
-        templateUrl: 'views/home.html'
-      })
-      .when("/addnewgoal", {
-        templateUrl: 'views/addnewgoal.html'
-      })
-      .otherwise({redirectTo:'/'});
+    $stateProvider
+      .state("welcome", {
+        url: "/",
+        templateUrl: 'views/signUp.html',
+        data: {
+          requireLogin: false
+        }
+      });
+      // .state("/addnewgoal", {
+      //   templateUrl: 'views/addnewgoal.html'
+      // })
+      // .otherwise({redirectTo:'/'});
   }]);
